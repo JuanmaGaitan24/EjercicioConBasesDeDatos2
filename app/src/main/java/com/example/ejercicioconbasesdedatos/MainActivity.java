f65d0ba19baeb18e7fffbf9b6aa1cafa7ffdc54c
JuanMa <juanmanuelgaitan@iesflorenciopintado.es>
2022-11-25 14:05:23 +0100

    ejercicio con bases de datos V2

--- app/src/main/java/com/example/ejercicioconbasesdedatos/MainActivity.java
+++ app/src/main/java/com/example/ejercicioconbasesdedatos/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.ejercicioconbasesdedatos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -14,12 +21,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.channels.AsynchronousChannelGroup;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,7 +46,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
         listaDatos = findViewById(R.id.listaDatos);
         btnCSV = findViewById(R.id.buttonCSV);
         btnXML = findViewById(R.id.buttonXML);
@@ -42,45 +57,138 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                ListarPorCSV();
+                DescargarCSV descargarCSV = new DescargarCSV();
+                descargarCSV.execute("listadoCSV.php");
+
+            }
+        });
+
+        btnXML.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DescargarXML descargarXML = new DescargarXML();
+                descargarXML.execute("listadoXML.php");
 
             }
         });
 
     }
 
-    private void ListarPorCSV() {
-
-        String url = SERVIDOR + "listadoCSV.php";
-        String linea = "";
-        String consulta = "";
-
-        try {
-
-            URLConnection conexion = null;
-            conexion = new URL(url).openConnection();
-            InputStream inputStream = conexion.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-
-            ArrayList<String> lineas = null;
-
-            while ((linea = br.readLine()) != null) {
-
-                lineas.add(linea);
-
-            }
-
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, lineas);
-            listaDatos.setAdapter(adapter);
-
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(MainActivity.class.getName()).log(Level.SEVERE, null, ex);
+    private class DescargarXML extends AsyncTask<String, Void, Void>{
+        String todo = "";
+        DefaultTableModel modelo;
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            modelo = new DefaultTableModel();
         }
 
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            try {
+                String url = SERVIDOR + "listadoXML.php";
+
+                DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+                DocumentBuilder db = dbf.newDocumentBuilder();
+
+                Document doc = db.parse(new URL(url).openStream());
+                Element raiz = doc.getDocumentElement();
+                System.out.println("Raíz: " + raiz.getNodeName());
+
+                NodeList hijos = doc.getElementsByTagName("articulo");
+
+                for (int i = 0; i < hijos.getLength(); i++) {
+                    Node nodo = hijos.item(i);
+
+                    if (nodo instanceof Element) {
+                        NodeList nietos = nodo.getChildNodes();
+                        String[] fila = new String[nietos.getLength()];
+                        for (int j = 0; j < nietos.getLength(); j++) {
+                            if (i == 0) {
+                                modelo.addColumn(nietos.item(j).getNodeName());
+                            }
+                            if (nietos.item(j) instanceof Element) {
+                                fila[j] = nietos.item(j).getTextContent();
+                            }
+                        }
+                        modelo.addRow(fila);
+                    }
+
+                }
+            } catch (ParserConfigurationException ex) {
+                Logger.getLogger(AccesoBD.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(AccesoBD.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(AccesoBD.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SAXException ex) {
+                Logger.getLogger(AccesoBD.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            return null;
+        }
+
+    }
+
+    private class DescargarCSV extends AsyncTask<String, Void, Void> {
+        String todo = "";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+            ArrayAdapter<String> adapter;
+            List<String> list = new ArrayList<String>();
+            String[] lineas = todo.split("\n");
+
+            for(String linea:lineas){
+                String[] campos = linea.split(";");
+                String dato="ID: "+campos[0];
+                dato+=" PRODUCTO: "+campos[1];
+                dato+=" MODELO: "+campos[2];
+                dato+=" PRECIO: "+campos[3];
+                list.add(dato);
+            }
+            adapter = new ArrayAdapter<String>(getApplicationContext(), androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, list);
+            listaDatos.setAdapter(adapter);
+
+        }
+
+        @Override
+        protected Void doInBackground(String... strings) {
+            String script = strings[0];
+            URL url;
+            HttpURLConnection httpURLConnection;
+
+            try {
+                url = new URL(SERVIDOR + script);
+
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                if (httpURLConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+                    String linea = "";
+                    while ((linea = br.readLine()) != null) {
+                        todo += linea+"\n";
+                    }
+                    br.close();
+                    inputStream.close();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 
 
